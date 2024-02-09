a1b2b042680f1c0540f65f0701fa78be75f1d900
Mark Rhey Anthony De Luna <delunamradf@gmail.com>
2024-02-09 23:52:48 +0800

    progress

--- src/App.java
+++ src/App.java
@@ -1,7 +1,55 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Grab Fleet Management");
-        System.out.println("try");
+     connect();
+    }
+
+    public static void connect(){
+        
+        String url = "jdbc:mysql://localhost:3306/midnightrain";
+        String user = "root";
+        String password = "";
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+            
+            String sqlQuery = "SELECT * FROM admin";
+            System.out.println("Connected to the database");
+            
+            resultSet = statement.executeQuery(sqlQuery);
+
+            while(resultSet.next()){
+                System.out.println(resultSet.getString("admin_FNAME"));
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally {
+           
+            try {
+                if (resultSet != null) resultSet.close();
+                if (statement != null) statement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
     }
 }
