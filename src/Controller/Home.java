88ca872caedc846d0257fff30ac4ee4ea966dbc9
Danyssa B. Tamayo <120694207+tamayodb@users.noreply.github.com>
2024-02-29 10:09:21 +0800

    Finalized Dashboard and Notification fxml
    -need database
    -handle notifications panel

--- src/Controller/Home.java
+++ src/Controller/Home.java
@@ -11,6 +11,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import javafx.scene.Node;
@@ -19,7 +20,26 @@ import javafx.scene.Scene;
 
 public class Home {
 
+    @FXML
+    public Circle  notificationCircleHome;
 
+    private Notification notificationController;
+
+    public void setNotificationController(Notification notificationController) {
+        this.notificationController = notificationController;
+        // Update the notification circle visibility based on the notification indicator status
+        updateNotificationCircleVisibility();
+    }
+
+    // Method to update the visibility of the notification circle
+    private void updateNotificationCircleVisibility() {
+        if (notificationController != null) {
+            boolean hasNotifications = notificationController.notificationCircle.isVisible();
+            notificationCircleHome.setVisible(hasNotifications);
+        }
+    }
+       
+    
     public void GoToC_Accounts(ActionEvent event) throws IOException {
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -40,5 +60,15 @@ public class Home {
             stage.setScene(scene);
             stage.show();
 
+    }
+
+    public void GoToNotif(ActionEvent event) throws IOException {
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/View/Notification.fxml"));
+       
+        Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
     }
 }
